e22f0d5c7f4281f87872aba24c8b0a8a6eb57541
Kratik1618 <77960953+Kratik1618@users.noreply.github.com>
2023-04-23 15:36:45 +0530

    Add files via upload

---
+++ Experiment-02.py
@@ -0,0 +1,26 @@
+#Kratik Pandey 0827CI201095
+#List Comprehension
+number_list = [ x for x in range(20) if x % 2 == 0]
+print(number_list)
+
+
+#Lambda function with List Comprehension
+#Kratik Pandey 0827CI201095
+#List Comprehension
+lettersList = list(map(lambda x: x, 'human'))
+print(lettersList)
+
+
+#Nested if with List Comprehension
+#Kratik Pandey 0827CI201095
+#List Comprehension
+num_list = [y for y in range(100) if y % 2 == 0 if y % 5 == 0]
+print(num_list)
+
+#Nested if..else with List Comprehension
+#Kratik Pandey 0827CI201095
+#List Comprehension
+obj = ["Even" if i%2==0 else "Odd" for i in range(10)]
+print(obj)
+
+
